3d418ef4302a55db16d0e60981edd2d410e39483
kavita <kavitapandey020994@gmail.com>
2023-07-10 13:22:54 +0100

    searching a number

---
+++ Script snippet #41.js
@@ -0,0 +1,26 @@
+var searchInsert = function(nums, target) {
+  let start = 0;
+  let end = nums.length - 1;
+  
+  while (start <= end) {
+    const middle = Math.floor((end + start) / 2);
+    
+    if (nums[middle] === target) {
+      return middle;
+    }
+    
+    if (nums[middle] < target) {
+      start = middle + 1
+      continue;
+    }
+      
+    if (nums[middle] > target) {
+      end = middle - 1;
+      continue;
+    }
+    
+    return middle;
+  }
+  
+  return start;
+};
